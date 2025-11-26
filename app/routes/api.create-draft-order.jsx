3f805a437e39761c0bb443a35dbe55922d5395bb
navjot <git@netgains.org>
2025-11-26 16:14:48 +0530

    mail info

--- app/routes/api.create-draft-order.jsx
+++ app/routes/api.create-draft-order.jsx
@@ -12,8 +12,8 @@ function getEmailTransporter() {
   if (!emailTransporter && process.env.SMTP_USER && process.env.SMTP_PASS) {
     emailTransporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
